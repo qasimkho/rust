59ccd7ec37e43d5ddd0444b7a9dfacdaa98601ec
MQ <muhammad.qasim1@live.com>
2025-01-19 22:27:28 -0500

    initial commit

---
+++ primitive.rs
@@ -0,0 +1,10 @@
+//compile -> rustc <filename>
+// run -> ./filename
+
+//OR compile -> rustc -o userDefinedName <filename>
+// run -> ./userDefinedName
+
+fn main(){
+
+    println!("hello world!");           
+}
